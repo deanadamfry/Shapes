58a527046fbf9e2da0aa61be49aae69d34fbeffa
Dean Fry <deanadamfry@gmail.com>
2021-12-29 17:00:30 +0000

    Updated Shapes to include multiple sizes of Circle and Square. Updated Characters to create and display PC and three NPCS. Project considered complete for now.

--- Shapes.cpp
+++ Shapes.cpp
@@ -19,14 +19,14 @@ class Circle {
 private:
 	float pi = 3.141593;
 public:
-	float m_radius = 0;
+	float m_Radius = 0;
 
 	float getArea() {
-		float circleArea = pi * (m_radius * m_radius);
+		float circleArea = pi * (m_Radius * m_Radius);
 		return circleArea;
 	}
 	float getCircumference() {
-		float circleCircumference = 2 * pi * m_radius;
+		float circleCircumference = 2 * pi * m_Radius;
 		return circleCircumference;
 	}
 };
@@ -36,10 +36,12 @@ public:
 	float m_SideLength = 0;
 
 	float getArea() {
-
+		float squareArea = m_SideLength * m_SideLength;
+		return squareArea;
 	}
 	float getPerimeter() {
-
+		float squarePerimeter = 4 * m_SideLength;
+		return squarePerimeter;
 	}
 };
 
@@ -52,11 +54,11 @@ public:
 	int m_XP = 0;               // Experience Points
 	string m_Name = "Hero";     // Name of Character
 	string m_Weapon = "Sword";  // Character Weapon
-	bool m_Armed = true;
-	bool m_IsAlive = true;
+	bool m_Armed = true;        // Character Armed?
+	bool m_IsAlive = true;      // Character Alive?
 
 	void checkIsAlive() {
-
+		// Workshop does not use this function
 	}
 };
 
@@ -119,12 +121,10 @@ void mainMenu()
 	{
 		cout << "\n\n ====== Shapes ====== \n\n";
 		cout << " Please Select a Program to run: \n";
-		cout << " 1: Circle Area \n";
-		cout << " 2: Circle Circumference \n";
-		cout << " 3: Square Area \n";
-		cout << " 4: Square Perimeter \n";
-		cout << " 5: Main Menu \n";
-		cout << " 6: Exit Program \n";
+		cout << " 1: Circle \n";
+		cout << " 2: Square \n";
+		cout << " 3: Main Menu \n";
+		cout << " 4: Exit Program \n";
 		cout << "\n : ";
 
 		cin >> playerChoice; // User Input
@@ -144,43 +144,59 @@ void mainMenu()
 			cin >> playerChoice;
 		}
 
-		Circle circle; // Circle Object
-		Square square; // Square Object
+		Circle circleSmall, circleMedium, circleLarge; // Circle Objects
+		Square squareSmall, squareMedium, squareLarge; // Square Objects
 
 		switch (playerChoice)
 		{
 		case 1:
 		{
-			cout << "\n Please input radius:";
-			cout << "\n : ";
-			cin >> circle.m_radius;
-			cout << " : " << circle.getArea(); 
+			circleSmall.m_Radius = 2;
+			cout << "\n Small Circle";
+			cout << "\n Area: " << circleSmall.getArea();
+			cout << "\n Circumference: " << circleSmall.getCircumference();
+			cout << "\n";
+			
+			circleMedium.m_Radius = 5;
+			cout << "\n Medium Circle";
+			cout << "\n Area: " << circleMedium.getArea();
+			cout << "\n Circumference: " << circleMedium.getCircumference();
+			cout << "\n";
+
+			circleLarge.m_Radius = 10;
+			cout << "\n Large Circle";
+			cout << "\n Area: " << circleLarge.getArea();
+			cout << "\n Circumference: " << circleLarge.getCircumference();
+			cout << "\n";
 		}
 		break;
 		case 2:
 		{
-			cout << "\n Please input radius:";
-			cout << "\n : ";
-			cin >> circle.m_radius;
-			cout << " : " << circle.getCircumference();
+			squareSmall.m_SideLength = 2;
+			cout << "\n Small Square";
+			cout << "\n Area: " << squareSmall.getArea();
+			cout << "\n Perimeter: " << squareSmall.getPerimeter();
+			cout << "\n";
+
+			squareMedium.m_SideLength = 5;
+			cout << "\n Medium Square";
+			cout << "\n Area: " << squareMedium.getArea();
+			cout << "\n Perimeter: " << squareMedium.getPerimeter();
+			cout << "\n";
+
+			squareLarge.m_SideLength = 10;
+			cout << "\n Large Square";
+			cout << "\n Area: " << squareLarge.getArea();
+			cout << "\n Perimeter: " << squareLarge.getPerimeter();
+			cout << "\n";
 		}
 		break;
 		case 3:
 		{
-			cout << "Option Unavailable";
+			// Leave blank, returns to Main Menu
 		}
 		break;
 		case 4:
-		{
-			cout << "Option Unavailable";
-		}
-		break;
-		case 5:
-		{
-			
-		}
-		break;
-		case 6:
 		{
 			gameOver = true;
 		}
@@ -192,12 +208,104 @@ void mainMenu()
 		break;
 	case 2:
 	{
-		cout << "Option Unavailable";
+		Character pc, npc_Steve, npc_Jessica, npc_Keith; // Character Objects
+
+		// Player Character Stats
+		pc.m_HP = 100;
+		pc.m_SPD = 5;
+		pc.m_STA = 5;
+		pc.m_STR = 5;
+		pc.m_XP = 0;
+		pc.m_Name = "Dean The Hero";
+		pc.m_Weapon = "Steel Longsword";
+		pc.m_Armed = true;
+		pc.m_IsAlive = true;
+
+		// Display Player Character Stats
+		cout << "\n Name: " << pc.m_Name;
+		cout << "\n Weapon: " << pc.m_Weapon;
+		cout << "\n HP: " << pc.m_HP;
+		cout << "\n Speed: " << pc.m_SPD;
+		cout << "\n Stamina: " << pc.m_STA;
+		cout << "\n Strength: " << pc.m_STR;
+		cout << "\n XP: " << pc.m_XP;
+		cout << "\n Armed?: " << pc.m_Armed;
+		cout << "\n Alive?: " << pc.m_IsAlive;
+		cout << "\n";
+
+		// Non-Playable Character Steve Stats
+		npc_Steve.m_HP = 0;
+		npc_Steve.m_SPD = 1;
+		npc_Steve.m_STA = 1;
+		npc_Steve.m_STR = 1;
+		npc_Steve.m_XP = 100;
+		npc_Steve.m_Name = "Steve The Merchant";
+		npc_Steve.m_Weapon = "None";
+		npc_Steve.m_Armed = false;
+		npc_Steve.m_IsAlive = false;
+
+		// Display NPC Steve Stats
+		cout << "\n Name: " << npc_Steve.m_Name;
+		cout << "\n Weapon: " << npc_Steve.m_Weapon;
+		cout << "\n HP: " << npc_Steve.m_HP;
+		cout << "\n Speed: " << npc_Steve.m_SPD;
+		cout << "\n Stamina: " << npc_Steve.m_STA;
+		cout << "\n Strength: " << npc_Steve.m_STR;
+		cout << "\n XP: " << npc_Steve.m_XP;
+		cout << "\n Armed?: " << npc_Steve.m_Armed;
+		cout << "\n Alive?: " << npc_Steve.m_IsAlive;
+		cout << "\n";
+
+		// Non-Playable Character Jessica Stats
+		npc_Jessica.m_HP = 50;
+		npc_Jessica.m_SPD = 3;
+		npc_Jessica.m_STA = 4;
+		npc_Jessica.m_STR = 3;
+		npc_Jessica.m_XP = 50;
+		npc_Jessica.m_Name = "Jessica The Farmgirl";
+		npc_Jessica.m_Weapon = "None";
+		npc_Jessica.m_Armed = false;
+		npc_Jessica.m_IsAlive = true;
+
+		// Display NPC Jessica Stats
+		cout << "\n Name: " << npc_Jessica.m_Name;
+		cout << "\n Weapon: " << npc_Jessica.m_Weapon;
+		cout << "\n HP: " << npc_Jessica.m_HP;
+		cout << "\n Speed: " << npc_Jessica.m_SPD;
+		cout << "\n Stamina: " << npc_Jessica.m_STA;
+		cout << "\n Strength: " << npc_Jessica.m_STR;
+		cout << "\n XP: " << npc_Jessica.m_XP;
+		cout << "\n Armed?: " << npc_Jessica.m_Armed;
+		cout << "\n Alive?: " << npc_Jessica.m_IsAlive;
+		cout << "\n";
+
+		// Non-Playable Character Keith Stats
+		npc_Keith.m_HP = 0;
+		npc_Keith.m_SPD = 5;
+		npc_Keith.m_STA = 5;
+		npc_Keith.m_STR = 5;
+		npc_Keith.m_XP = 500;
+		npc_Keith.m_Name = "Keith The Thief";
+		npc_Keith.m_Weapon = "Iron Dagger";
+		npc_Keith.m_Armed = true;
+		npc_Keith.m_IsAlive = false;
+
+		// Display NPC Keith Stats
+		cout << "\n Name: " << npc_Keith.m_Name;
+		cout << "\n Weapon: " << npc_Keith.m_Weapon;
+		cout << "\n HP: " << npc_Keith.m_HP;
+		cout << "\n Speed: " << npc_Keith.m_SPD;
+		cout << "\n Stamina: " << npc_Keith.m_STA;
+		cout << "\n Strength: " << npc_Keith.m_STR;
+		cout << "\n XP: " << npc_Keith.m_XP;
+		cout << "\n Armed?: " << npc_Keith.m_Armed;
+		cout << "\n Alive?: " << npc_Keith.m_IsAlive;
+		cout << "\n";
 	}
 		break;
 	case 3:
 	{
-		cout << "Option Unavailable";
+		cout << "\n Option Unavailable";
 	}
 		break;
 	case 4:
